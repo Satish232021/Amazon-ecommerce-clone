7d515858b9b5539a4b893ff85b255bca6deee1e7
Satish232021 <satishreddy2314@gmail.com>
2024-09-16 01:53:50 +0530

    Loade products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,18 @@
 import { renderOrderSummary } from "../../scripts/checkouot/orderSummary.js";
 import {loadFromStorage, cart} from '../../data/cart.js';
+import { loadProducts } from "../../data/products.js";
 
 
 describe('test  suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+
+    beforeAll((done) => {
+        loadProducts(() => {
+            done(); /* done() function allowa us to control when to go to the next step....if we not use this we keep waiting forever */
+        }); /* this make sure that the products load first and we call done and then mve to the further steps of code */
+    });/* we can also use the done inside beforeEach((done) => { ..}) or it((done ) => {...}) to waait for the response from thebackend and to move to the further steps of code*/
 
     beforeEach(() => {
         spyOn(localStorage, 'setItem');
