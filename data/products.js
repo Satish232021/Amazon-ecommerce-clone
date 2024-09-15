1d8e90a5ce48bcfb95051e29a45e97d1db806a33
Satish232021 <satishreddy2314@gmail.com>
2024-09-16 01:41:26 +0530

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -86,7 +86,31 @@ class Clothing extends Product{ /* Here CLothing class will inherit { inheritanc
 // logThis();
 // logThis.call('hello'); /* it is same as above ..but call( 'hello' ) will be accessed by this and print hello as it was outer */
 
-export const products = [
+
+export let products =[];
+
+export function loadProducts (fun) { /* here fun is called as a callback----> a function to run in the future */
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+  
+}
+
+
+/* export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -750,7 +774,7 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
-});
+}); */
 
 
 // console.log(products);/* check this for clarification */
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,12 @@
 import { renderOrderSummary } from './checkouot/orderSummary.js';
 import { renderPaymentSummary } from './checkouot/paymentSummary.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+
+
+loadProducts(() => { /* we dont need to write a function everytime..we can also use anonymous function { function without name }*/
+    renderOrderSummary();
+    renderPaymentSummary();
+});
